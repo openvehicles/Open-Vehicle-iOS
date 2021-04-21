a6663d464e3f9f7cc7614313448fcc3115ba101f
Peter Harry <p.harry@me.com>
2021-04-21 11:32:58 +1000

    Changed car_minutestorangelimit to correct item in received message

--- OpenVehicleApp/ovms/ovmsAppDelegate.m
+++ OpenVehicleApp/ovms/ovmsAppDelegate.m
@@ -722,12 +722,12 @@
       if ([lparts count]>=23)
         {
         car_minutestofull = [[lparts objectAtIndex:19] intValue];
-        car_minutestorangelimit = [[lparts objectAtIndex:20] intValue];
         car_rangelimit = [[lparts objectAtIndex:21] intValue];
         car_soclimit = [[lparts objectAtIndex:22] intValue];
         }
       if([lparts count]>=31)
         {
+        car_minutestorangelimit = [[lparts objectAtIndex:27] intValue];
         car_minutestosoclimit = [[lparts objectAtIndex:28] intValue];
         car_chargetype = [[lparts objectAtIndex:30] intValue];
         }
